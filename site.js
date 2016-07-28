3b0f26ae5368f8b8a8d5e3579eac98ab18af3f38
Daniel Beecham <daniel@lunix.se>
2016-07-28 10:57:52 +0000

    Added semicolons.

--- site.js
+++ site.js
@@ -1,16 +1,17 @@
 /*jslint
   indent:2
 */
+/*global $, jQuery */
 (function() {
   'use strict';
   
   // Micro templating
   var render = function(string, data) {
     for(var s in data) {
-      string = string.replace(new RegExp('{'+s+'}', 'g'), data[s])
+      string = string.replace(new RegExp('{'+s+'}', 'g'), data[s]);
     }
-    return string
-  }
+    return string;
+  };
   
   // Create a template function bound to a
   // template string ready for rendering data
@@ -19,20 +20,20 @@
   //        => 'Hi Johan'
   var template = function(string) {
     'use strict';
-    return render.bind(this, string)
-  }
+    return render.bind(this, string);
+  };
   
   var getExercises = function() {
     'use strict';
     return $.getJSON('exercises.json').then(function(res) {
-      return res
-    })
-  }
+      return res;
+    });
+  };
   
   var getReadmeForExercise = function(exercise) {
     'use strict';
-    return $.get('exercises/' + exercise.name + '/README.html')
-  }
+    return $.get('exercises/' + exercise.name + '/README.html');
+  };
   
   // Matches:
   //   Svårighetsgrad: <level>
@@ -41,37 +42,37 @@
   //   etc.
   var extractLevel = function(content) {
     'use strict';
-    var matches = content.match(/Svårighetsgrad[\D]*(\d)/i)
-    return matches ? matches[1] : false
-  }
+    var matches = content.match(/Svårighetsgrad[\D]*(\d)/i);
+    return matches ? matches[1] : false;
+  };
   
   // Create a plain object for templating
   var transformExercise = function(exercise, i) {
     'use strinct';
-    var content = exercise.readme
-    var files = exercise.files
-    var name = $(content).filter("h1").first().text()
+    var content = exercise.readme;
+    var files = exercise.files;
+    var name = $(content).filter("h1").first().text();
   
     // Include listing of attached files if there are any
     if (files.length > 0) {
   
-      var items = []
+      var items = [];
       files.forEach(function(file) {
-        items.push('<a href="exercises/' + exercise.name + '/' + file + '" download>' + file + '</a>')
-      })
+        items.push('<a href="exercises/' + exercise.name + '/' + file + '" download>' + file + '</a>');
+      });
   
-      var filesText = '\n<h2>Filer</h2>\n' + items.join(", ")
+      var filesText = '\n<h2>Filer</h2>\n' + items.join(", ");
   
       // Inject list of files before first h2, or at the end if there are none
       var $html = $('<div />', { html: content }),
           h2s = $html.find('h2');
           
       if (h2s.length > 0) {
-        h2s.first().before(filesText)
+        h2s.first().before(filesText);
       } else {
-        $html.append(filesText)
+        $html.append(filesText);
       }
-      content = $html.html()
+      content = $html.html();
     }
   
     return {
@@ -79,28 +80,28 @@
       level: extractLevel(content) || 'Okänd',
       text: name, // The first heading
       content: content
-    }
-  }
+    };
+  };
   
   // Comparator function for exercises
   var sortByLevel = function(exercises) {
     'use strict';
     return exercises.sort(function(e1, e2) {
       return (e1.level > e2.level) ? 1 :
-        (e1.level < e2.level) ? -1 : 0
-    })
-  }
+        (e1.level < e2.level) ? -1 : 0;
+    });
+  };
   
   // Build the list from an exercises object array
   var renderReadmeList = function(exercises) {
     'use strict';
     var $container = $(".exercises-list"),
-        tmpl = template($("#exercise-template").html())
+        tmpl = template($("#exercise-template").html());
   
     // Render each exercise with 'tmpl', whose only argument
     // is a data object (given in Array.map).
-    return $container.html( exercises.map(tmpl).join("") )
-  }
+    return $container.html( exercises.map(tmpl).join("") );
+  };
   
   // Custom function mimicing Q.all():
   // returns a promise whose argument is
@@ -108,15 +109,15 @@
   // and array of promises
   var whenAll = function(promises) {
     'use strict';
-    var slice = Array.prototype.slice
+    var slice = Array.prototype.slice;
     return $.when.apply($, promises).then(function() {
       return slice.call(arguments).map(function(r) {
         // The first element is the data object
         // The others are the status and the XHR promise itself.
-        return r[0]
-      })
-    })
-  }
+        return r[0];
+      });
+    });
+  };
   
   // Fetch READMEs and build list
   var fetchReadmes = function() {
@@ -131,28 +132,28 @@
         // Add readme to each exercise
         // Could probably be done more elegantly
   
-        var dfd = $.Deferred()
+        var dfd = $.Deferred();
         // Modify exercise object when all README-fetches have settled
         whenAll(exercises.map(getReadmeForExercise)).then(function(readmes) {
           readmes.forEach(function(readme, i) {
-            exercises[i].readme = readme
-          })
-          dfd.resolve(exercises)
-        })
-        return dfd
+            exercises[i].readme = readme;
+          });
+          dfd.resolve(exercises);
+        });
+        return dfd;
       })
       
       // Transform each exercise (parse out relevant data for templating)
       .then(function(exercises) {
-        return exercises.map(transformExercise)
+        return exercises.map(transformExercise);
       })
   
       // Sort by difficulty level
       .then(sortByLevel)
   
       // Render the exercises with README content
-      .then(renderReadmeList)
-  }
+      .then(renderReadmeList);
+  };
   
   var buildNavigation = (function($) {
     'use strict';
@@ -161,13 +162,13 @@
       container: "body",
       nodes: "h1",
       navigation: "[role='navigation'] ul"
-    }
+    };
   
     return function(options) {
-      options = $.extend({}, defaults, options)
+      options = $.extend({}, defaults, options);
   
       var headings = $(options.container).find(options.nodes),
-          frag = []
+          frag = [];
   
       headings.each(function(i, item) {
         if(!item.id) return;
@@ -178,30 +179,30 @@
           href: "#"+item.id,
           text: $(item).text(),
           "data-scroll": true
-        })
+        });
   
-        li.append(a)
-        frag.push(li)
-      })
+        li.append(a);
+        frag.push(li);
+      });
   
-      $(options.navigation).html(frag)
-    }
+      $(options.navigation).html(frag);
+    };
   
-  })(jQuery)
+  })(jQuery);
   
   $(function() {
     'use strict';
   
-    fetchReadmes()
+    fetchReadmes();
   
     buildNavigation({
       container: "[role='main']"
-    })
+    });
   
     $("#accordion").on("shown.bs.collapse", function(evt) {
-      var panel = $(evt.target)
-    })
+      var panel = $(evt.target);
+    });
   
-  })
+  });
 
-})()
+})();
