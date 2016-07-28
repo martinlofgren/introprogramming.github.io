0f564948dd0d9594dc039a4f1fcef6a082e49d3e
Daniel Beecham <daniel@lunix.se>
2016-07-28 11:35:41 +0000

    General style improvements.

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
 /*jslint
-  indent:2
+  indent:2, devel:true
 */
 /*global $, jQuery */
 
@@ -20,28 +20,31 @@
 
   // Micro templating
   render = function (string, data) {
+    // TODO: for in is not a good idea in javascript; it's slow and may recurse
+    // to arbitrary depths up the prototype chain.
     for (s in data) {
-      string = string.replace(new RegExp('{'+s+'}', 'g'), data[s]);
+      console.log(s);
+      string = string.replace(new RegExp('{' + s + '}', 'g'), data[s]);
     }
     return string;
   };
-  
+
   // Returns a function with an argument already applied; a partially applied
   // function.
   partial = function (f, argument) {
     return f.bind(this, argument);
   };
-  
+
   getExercises = function () {
-    return $.getJSON('exercises.json').then(function(res) {
+    return $.getJSON('exercises.json').then(function (res) {
       return res;
     });
   };
-  
+
   getReadmeForExercise = function (exercise) {
     return $.get('exercises/' + exercise.name + '/README.html');
   };
-  
+
   // Matches:
   //   Svårighetsgrad: <level>
   //   **Svårighetsgrad: **<level>
@@ -51,27 +54,30 @@
     var matches = content.match(/Svårighetsgrad[\D]*(\d)/i);
     return matches ? matches[1] : false;
   };
-  
+
   // Create a plain object for templating
   transformExercise = function (exercise, i) {
-    var content = exercise.readme;
-    var files = exercise.files;
-    var name = $(content).filter("h1").first().text();
-  
+    var content = exercise.readme,
+      files = exercise.files,
+      name = $(content).filter("h1").first().text(),
+      items = [],
+      filesText,
+      $html,
+      h2s;
+
     // Include listing of attached files if there are any
     if (files.length > 0) {
-  
-      var items = [];
+
       files.forEach(function (file) {
         items.push('<a href="exercises/' + exercise.name + '/' + file + '" download>' + file + '</a>');
       });
-  
-      var filesText = '\n<h2>Filer</h2>\n' + items.join(", ");
-  
+
+      filesText = '\n<h2>Filer</h2>\n' + items.join(", ");
+
       // Inject list of files before first h2, or at the end if there are none
-      var $html = $('<div />', { html: content }),
-          h2s = $html.find('h2');
-          
+      $html = $('<div />', { html: content });
+      h2s = $html.find('h2');
+
       if (h2s.length > 0) {
         h2s.first().before(filesText);
       } else {
@@ -79,7 +85,7 @@
       }
       content = $html.html();
     }
-  
+
     return {
       order: i,
       level: extractLevel(content) || 'Okänd',
@@ -87,118 +93,119 @@
       content: content
     };
   };
-  
+
   // Comparator function for exercises
   sortByLevel = function (exercises) {
-    return exercises.sort(function(e1, e2) {
+    return exercises.sort(function (e1, e2) {
       return (e1.level > e2.level) ? 1 :
-        (e1.level < e2.level) ? -1 : 0;
+          (e1.level < e2.level) ? -1 : 0;
     });
   };
-  
+
   // Build the list from an exercises object array
   renderReadmeList = function (exercises) {
     var $container = $(".exercises-list"),
       tmpl = partial(render, $("#exercise-template").html());
-  
+
     // Render each exercise with 'tmpl', whose only argument
     // is a data object (given in Array.map).
-    return $container.html( exercises.map(tmpl).join("") );
+    return $container.html(exercises.map(tmpl).join(""));
   };
-  
+
   // Custom function mimicing Q.all():
   // returns a promise whose argument is
   // an array with all data objects. Takes
   // and array of promises
   whenAll = function (promises) {
     var slice = Array.prototype.slice;
-    return $.when.apply($, promises).then(function() {
-      return slice.call(arguments).map(function(r) {
+    return $.when.apply($, promises).then(function () {
+      return slice.call(arguments).map(function (r) {
         // The first element is the data object
         // The others are the status and the XHR promise itself.
         return r[0];
       });
     });
   };
-  
+
   // Fetch READMEs and build list
   fetchReadmes = function () {
     // Fetch exercises
     getExercises()
-  
+
       // Then for each exercise, fetch its README
-      .then(function(exercises) {
-  
+      .then(function (exercises) {
+
         // Add readme to each exercise
         // Could probably be done more elegantly
-  
+
         var dfd = $.Deferred();
         // Modify exercise object when all README-fetches have settled
-        whenAll(exercises.map(getReadmeForExercise)).then(function(readmes) {
-          readmes.forEach(function(readme, i) {
+        whenAll(exercises.map(getReadmeForExercise)).then(function (readmes) {
+          readmes.forEach(function (readme, i) {
             exercises[i].readme = readme;
           });
           dfd.resolve(exercises);
         });
         return dfd;
       })
-      
+
       // Transform each exercise (parse out relevant data for templating)
-      .then(function(exercises) {
+      .then(function (exercises) {
         return exercises.map(transformExercise);
       })
-  
+
       // Sort by difficulty level
       .then(sortByLevel)
-  
+
       // Render the exercises with README content
       .then(renderReadmeList);
   };
-  
-  buildNavigation = (function($) {
+
+  buildNavigation = (function ($) {
     var defaults = {
       container: "body",
       nodes: "h1",
       navigation: "[role='navigation'] ul"
     };
-  
-    return function(options) {
+
+    return function (options) {
       options = $.extend({}, defaults, options);
-  
+
       var headings = $(options.container).find(options.nodes),
-          frag = [];
-  
-      headings.each(function(i, item) {
-        if(!item.id) return;
-  
+        frag = [];
+
+      headings.each(function (ignore, item) {
+        if (!item.id) {
+          return;
+        }
+
         var li = $("<li />"),
-  
-        a = $("<a />", {
-          href: "#"+item.id,
-          text: $(item).text(),
-          "data-scroll": true
-        });
-  
+
+          a = $("<a />", {
+            href: "#" + item.id,
+            text: $(item).text(),
+            "data-scroll": true
+          });
+
         li.append(a);
         frag.push(li);
       });
-  
+
       $(options.navigation).html(frag);
     };
-  
-  })(jQuery);
-  
-  $(function() {
+
+  }(jQuery));
+
+  $(function () {
     fetchReadmes();
-  
+
     buildNavigation({
       container: "[role='main']"
     });
-  
-    $("#accordion").on("shown.bs.collapse", function(evt) {
+
+    $("#accordion").on("shown.bs.collapse", function (evt) {
       var panel = $(evt.target);
     });
-  
-  });
 
-})();
+  });
+}());
